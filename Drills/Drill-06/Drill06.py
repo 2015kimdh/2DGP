3e98c6219714c6bb702afabc84a8679964c41589
2015kimdh <oms2385@naver.com>
2018-09-20 17:16:00 +0900

    error in calculating x,y

--- Drills/Drill-06/Drill06.py
+++ Drills/Drill-06/Drill06.py
@@ -5,15 +5,38 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 def handle_events():
     global running
+    global mx, my
+    global lx, ly
     global x, y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
-            x,y = event.x, KPU_HEIGHT - 1 - event.y
+            mx,my = event.x, KPU_HEIGHT - 1 - event.y
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            if SDL_BUTTON_LEFT:
+                lx = mx
+                ly = my
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
+
+def calculate():
+    global lx, ly
+    global x, y
+    if x != lx and y != ly:
+        if x > lx:
+            x -= 2
+        elif x < lx:
+            x += 2
+        if (ly - y)/(lx - x)*2 >= 1 or (ly - y)/(lx - x)*2 <= -1:
+            y += (ly - y)/(lx - x)
+        elif (ly - y)/(lx - x)*2 < 1 and (ly - y)/(lx - x)*2 >= 0:
+            y += 1
+        elif (ly - y) / (lx - x) * 2 > -1 and (ly - y)/(lx - x)*2 <= 0:
+            y -= 1
+    elif x == lx and y == ly:
+        pass
 
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
@@ -21,6 +44,8 @@ character = load_image('animation_sheet.png')
 
 running = True
 x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+lx, ly = 0, 0
+mx, my = 0, 0
 frame = 0
 show_cursor()
 
@@ -31,9 +56,9 @@ while running:
     update_canvas()
     frame = (frame + 1) % 8
 
-    delay(0.0)
+    delay(0.01)
     handle_events()
-
+    calculate()
 close_canvas()
 
 
