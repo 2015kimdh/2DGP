89673678e5ba0812600f854dadd0bcb47adc8cc6
2015kdh <oms2385@naver.com>
2018-10-01 17:19:36 +0900

    drill07 done
    character run finished

--- Drills/Drill-07/drill-07.py
+++ Drills/Drill-07/drill-07.py
@@ -3,21 +3,36 @@ import random
 
 open_canvas()
 
-grass = load_image('grass.png')
+kpu = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 
-def draw_line(p1, p2):
-
+def make_line(p1, p2):
+    frame = 0
+    clear_canvas_now()
     for i in range(0, 100 + 1, 2):
         t = i / 100
         x = (1 - t) * p1[0] + t * p2[0]
         y = (1 - t) * p1[1] + t * p2[1]
+        kpu.draw_now(300, 100)
+        if p2[0] - p1[0] > 0:
+            character.clip_draw(frame * 100, 100, 100, 100, x, y)
+        else :
+            character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        update_canvas()
+        frame += 1
+        frame = frame % 8
+        delay(0.01)
+        get_events()
+
 
 size = 20
-points = [(random.randint(-500, 500), random.randint(-400, 350)) for i in range(size)]
+points = [(random.randint(100, 700), random.randint(100, 600)) for i in range(size)]
 n = 1
+
 while True:
-    draw_line(points[n-1], points[n])
+
+    make_line(points[n-1], points[n])
     n = (n + 1) % size
+
 
 close_canvas()
